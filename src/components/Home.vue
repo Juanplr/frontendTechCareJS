e665387fb2200466e7068fa80bf60866a7d687df
Soka_jplr <juanlazarorp@gmail.com>
2024-05-15 12:48:35 -0600

    proyectoSW 1.0

---
+++ src/components/Home.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <input type="text" list="browsers"/>
+        <datalist id="browsers">
+            <option>Google</option>
+            <option>IE9</option>
+        </datalist>
+        <input type="text">
+        <input type="text">
+        <input type="date">
+        <input type="text">
+    </div>
+    <div>
+        <input type="text">
+        <input type="text">
+        <input type="text">
+        <input type="button">Generar Orden<input/>
+    </div>
+</template>
