c09e540dcb4859a595558acb4c4a24e80bf93093
Frantatita <reyes.23.jacome@gmail.com>
2024-05-21 19:04:28 -0600

    Interfaz Home

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -5,8 +5,13 @@
       <input class="id" type="text" />
       <button class="search">Nueva Orden de Servicio</button>
     </div>
-    <div>
+    <div class="container_image">
       <img class="image_logo" src="/Techcare_Claro.jpeg"/>
+    </div>
+    <div class="chat_container">
+      <div class="chat_bubble">
+        <p>Hola, ¿en qué puedo ayudarte?</p>
+      </div>
     </div>
   </div>
 </template>
