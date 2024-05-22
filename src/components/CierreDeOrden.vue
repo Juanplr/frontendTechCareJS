c7107e9926f7b977a783fad98cb2cb45f5abf2c8
Soka_jplr <juanlazarorp@gmail.com>
2024-05-21 20:41:57 -0600

    cierre de Orden

---
+++ src/components/CierreDeOrden.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="container_c">
+        <div class="container_nav">
+            <h6 id="h_No">No. de Orden #</h6>
+            <h6 id="h_titulo">Cierre de Orden de Servicio</h6>
+        </div>
+        <div class="form_principal">
+            <form class="form_orden">
+                <div class="container_left">
+                    <ul class="ul_left">
+                        <li class="li_left">
+                            <input type="text" class="input_text_line" placeholder="Nombre del Cliente"/>
+                        </li>
+                        <li class="li_left">
+                            <input type="text" class="input_text_line" placeholder="Servicio Brindado"/>
+                        </li>
+                        <li class="li_left">
+                            <input type="text" class="input_text_line" placeholder="Anticipo"/>
+                        </li>
+                        <li class="li_left">
+                            <input type="text" class="input_text_line" placeholder="Restante" />
+                        </li>
+                        <li class="li_left">
+                            <h6 class="h_left">Acciones realizadas en el servicio</h6>
+                            <textarea  class="input_text"></textarea>
+                        </li>
+                        <li class="li_left">
+                            <h6 class="h_left">Recomendaciones</h6>
+                            <textarea  class="input_text"></textarea>
+                        </li>
+                    </ul>
+                </div>
+                <div class="container_right">
+                    <ul class="ul_right">
+                        <li class="conclusiones">
+                            <h6>Conclusiones del Servicio</h6>
+                            <textarea  class="input_text"></textarea>
+                        </li>
+                        <li class="button">
+                            <button id="btn_orden">Generar Orden</button>
+                        </li>
+                    </ul>
+                </div>
+            </form>
+        </div>
+    </div>
+</template>
+
+  
+<style>
+@import "../assets/CierreDeOrden.css";
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,15 @@
-import {createRouter, createWebHistory} from 'vue-router';
+import { createRouter, createWebHistory } from 'vue-router';
 import Login from '../components/Login.vue';
 import Home from '../components/Home.vue';
 import FormularioOrden from '../components/FormularioOrden.vue';
+import CierreDeOrden from '@/components/CierreDeOrden.vue';
+
 
 const routes = [
-    {path: '/', component: Login},
-    {path: '/home', component: Home},
-    {path: '/formularioOrden', component: FormularioOrden}
+    { path: '/', component: Login },
+    { path: '/home', component: Home },
+    { path: '/formularioOrden', component: FormularioOrden },
+    { path: '/cierreOrden', component: CierreDeOrden }
 ];
 
 const router = createRouter({
