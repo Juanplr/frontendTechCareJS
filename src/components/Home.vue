6d46904fa56c4f0d2c985c5ae49874e933c00ed6
Soka_jplr <juanlazarorp@gmail.com>
2024-05-25 00:45:59 -0600

    cambios comflictos

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,18 +1,14 @@
 <script>
-<<<<<<< HEAD
+
 import { ref } from "vue";
 import { reactive } from 'vue';
 import axios from "axios";
 import {useRouter} from 'vue-router';
-=======
-import { useRouter } from 'vue-router';
-import { ref } from 'vue';
->>>>>>> d6004334fb3cdbcb12d4cdb0dcf79cafeb1dd7f0
+
 
 export default {
   setup() {
     const router = useRouter();
-<<<<<<< HEAD
     const id_orden_de_servicio_find = ref("");
     const formData = reactive({
       id_orden_de_servicio: ""
@@ -38,35 +34,7 @@ export default {
     };
 
     return { go_to_Formulario_orden, go_to_cierre_orden, formData, id_orden_de_servicio_find};
-=======
-    const notificationVisible = ref(false);
 
-    const go_to_Formulario_orden = () => {
-      router.push("/formularioOrden");
-    };
-
-    const buscarOrdenServicio = () => {
-      const inputId = document.querySelector('.id').value.trim();
-
-      // Validar que el campo de entrada no esté vacío
-      if (inputId === '') {
-        mostrarNotificacion();
-        return;
-      }
-
-      // Realizar la acción de búsqueda de la orden de servicio
-      // Aquí puedes agregar la lógica para buscar la orden de servicio con el ID proporcionado
-    };
-
-    const mostrarNotificacion = () => {
-      notificationVisible.value = true;
-      setTimeout(() => {
-        notificationVisible.value = false;
-      }, 3000);
-    };
-
-    return { go_to_Formulario_orden, buscarOrdenServicio, notificationVisible };
->>>>>>> d6004334fb3cdbcb12d4cdb0dcf79cafeb1dd7f0
   },
 };
 </script>
@@ -75,15 +43,9 @@ export default {
 <template>
   <div>
     <div class="header">
-<<<<<<< HEAD
       <button class="search" v-on:click="go_to_cierre_orden()">Buscar Orden de Servicio</button>
       <input class="id" type="text" v-model="id_orden_de_servicio_find" />
       <button type="button"class="search" v-on:click="go_to_Formulario_orden()">Nueva Orden de Servicio</button>
-=======
-      <button type="button" class="search" v-on:click="buscarOrdenServicio">Buscar Orden de Servicio</button>
-      <input class="id" type="text" />
-      <button type="button" class="search" v-on:click="go_to_Formulario_orden">Nueva Orden de Servicio</button>
->>>>>>> d6004334fb3cdbcb12d4cdb0dcf79cafeb1dd7f0
     </div>
     <div class="container_image">
       <img class="image_logo" src="/Techcare_Claro.jpeg"/>
