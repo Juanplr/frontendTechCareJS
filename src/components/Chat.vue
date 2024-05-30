c674edb790add117c5bb72d4a5c16ad498120815
Soka_jplr <juanlazarorp@gmail.com>
2024-05-29 20:58:00 -0600

    primera version de webSocket

---
+++ src/components/Chat.vue
@@ -0,0 +1,140 @@
+<template>
+  <div>
+    <!--<input
+      v-model="username"
+      class="txt-username"
+      type="text"
+      placeholder="username"
+    />
+
+    <div class="div-chat">
+      <p v-for="(message, idx) in listMessages" :key="idx">
+        {{ message.user }}: {{ message.body }}
+      </p>
+    </div>
+
+    <form @submit.prevent="handleSubmit" class="form">
+      <span class="title">Chat-io</span>
+      <p class="description">Type your message.</p>
+      <div class="div-type-chat">
+        <input
+          v-model="message"
+          placeholder="Type your message"
+          type="text"
+          name="text"
+          id="chat-message"
+          class="input-style"
+        />
+        <button type="submit">Send</button>
+      </div>
+    </form>-->
+    <div>Hola</div>
+    <div>Esto es un chat</div>
+  </div>
+</template>
+
+<script>
+import { ref, onMounted, onBeforeUnmount } from "vue";
+import io from "socket.io-client";
+
+const socket = io("http://localhost:3000/");
+
+export default {
+  name: 'Chat',
+  setup() {
+    /*const message = ref("");
+    const username = ref("");
+    const listMessages = ref([
+      { body: "Welcome to the chat room", user: username },
+    ]);
+
+
+    const handleSubmit = () => {
+      socket.emit("message", { body: message.value, user: username.value });
+      const newMsg = {
+        body: message.value,
+        user: username.value,
+      };
+      listMessages.value.push(newMsg);
+      message.value = "";
+    };
+
+    const receiveMessage = (msg) => {
+      listMessages.value.push(msg);
+    };
+
+    onMounted(() => {
+      socket.on("message", (msg) => {
+        if (
+          !listMessages.value.some(
+            (message) => message.body === msg.body && message.user === msg.user
+          )
+        ) {
+          receiveMessage(msg);
+        }
+      });
+    });
+
+    onBeforeUnmount(() => {
+      socket.off("message", receiveMessage);
+    });
+
+    return {
+      message,
+      username,
+      showPicker,
+      listMessages,
+      handleSubmit,
+      onEmojiClick,
+      toggleEmojiPicker,
+    };*/
+  },
+};
+</script>
+
+<style scoped>
+/*.txt-username {
+  width: 100%;
+  padding: 10px;
+  margin-bottom: 10px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+}
+
+.div-chat {
+  border: 1px solid #ccc;
+  padding: 10px;
+  margin-bottom: 10px;
+  height: 200px;
+  overflow-y: scroll;
+}
+
+.form {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.title {
+  font-size: 24px;
+  margin-bottom: 10px;
+}
+
+.description {
+  margin-bottom: 20px;
+}
+
+.div-type-chat {
+  display: flex;
+  align-items: center;
+}
+
+
+.input-style {
+  flex: 1;
+  padding: 10px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  margin-right: 10px;
+}*/
+</style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,42 +1,43 @@
 <script>
-
-import { ref } from "vue";
-import { reactive } from 'vue';
+import { ref, onMounted } from "vue";
+import { reactive } from "vue";
 import axios from "axios";
-import {useRouter} from 'vue-router';
-
+import { useRouter } from "vue-router";
+import Chat from '@/components/Chat.vue'
 
 export default {
-  setup() {
+  components: {
+    Chat
+  },setup() {
     const router = useRouter();
     const id_orden_de_servicio_find = ref("");
     const formData = reactive({
       id_orden_de_servicio: ""
     });
+    const isChatVisible = ref(false);
 
     const go_to_Formulario_orden = () => {
       console.log('Formulario enviado', formData.id_orden_de_servicio);
       router.push('/formularioOrden'); 
     };
 
-    const go_to_cierre_orden = () =>{
+    const go_to_cierre_orden = () => {
       const id = parseInt(id_orden_de_servicio_find.value);
-     
-      if (!id_orden_de_servicio_find.value || isNaN(id) || id<1){
-        alert("por favor ingrese una orden de servicio valido")
-        return
+      if (!id_orden_de_servicio_find.value || isNaN(id) || id < 1) {
+        alert("Por favor ingrese una orden de servicio válida");
+        return;
       }
-
-        
-        router.push('/cierreOrden');
-      
-      
+      router.push('/cierreOrden');
     };
 
-    return { go_to_Formulario_orden, go_to_cierre_orden, formData, id_orden_de_servicio_find};
+    const toggleChat = () => {
+      isChatVisible.value = !isChatVisible.value;
+    };
 
-  },
+    return { go_to_Formulario_orden, go_to_cierre_orden, formData, id_orden_de_servicio_find, toggleChat, isChatVisible};
+  }
 };
+
 </script>
 
 
@@ -50,11 +51,15 @@ export default {
     <div class="container_image">
       <img class="image_logo" src="/Techcare_Claro.jpeg"/>
     </div>
+    <div class="chat_contenedor" v-if="isChatVisible">
+      <Chat/>
+    </div>
     <div class="chat_container">
-      <div class="chat_bubble">
+      <div id="btn_chat" class="chat_bubble" @click="toggleChat">
         <p>Hola, ¿en qué puedo ayudarte?</p>
       </div>
     </div>
+
 
     <transition name="fade">
       <div v-if="notificationVisible" class="notification">
